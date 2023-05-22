14a6c4c17a3077564178ee0659157f5bf1a88e3e
funera1 <funeral7776@gmail.com>
2023-05-23 02:35:44 +0900

    Update

--- getRss.c
+++ getRss.c
@@ -9,6 +9,7 @@
 #include <linux/mm.h>
 #include <linux/mm_types.h>
 #include <linux/pid.h>
+#include <linux/task_mmu.c>
 #include "getRss.h"
 
 MODULE_LICENSE("GPL v2");
@@ -27,6 +28,41 @@ static int module_close(struct inode *inode, struct file *filp) {
   return 0;
 }
 
+static void my_smap_gather_stats(struct vm_area_struct *vma,
+        struct mem_size_stats *mss, unsigned long start, unsigned long end)
+{
+	const struct mm_walk_ops *ops = &smaps_walk_ops;
+
+	/* Invalid start */
+	if (start >= vma->vm_end)
+		return;
+
+#ifdef CONFIG_SHMEM
+	if (vma->vm_file && shmem_mapping(vma->vm_file->f_mapping)) {
+		/*
+		 * For shared or readonly shmem mappings we know that all
+		 * swapped out pages belong to the shmem object, and we can
+		 * obtain the swap value much more efficiently. For private
+		 * writable mappings, we might have COW pages that are
+		 * not affected by the parent swapped out pages of the shmem
+		 * object, so we have to distinguish them during the page walk.
+		 * Unless we know that the shmem object (or the part mapped by
+		 * our VMA) has no swapped out pages at all.
+		 */
+		unsigned long shmem_swapped = shmem_swap_usage(vma);
+
+		if (!start && (!shmem_swapped || (vma->vm_flags & VM_SHARED) ||
+					!(vma->vm_flags & VM_WRITE))) {
+			mss->swap += shmem_swapped;
+		} else {
+			ops = &smaps_shmem_walk_ops;
+		}
+	}
+#endif
+	/* mmap_lock is held in m_start */
+    walk_page_range(vma->vm_mm, start, end, ops, mss);
+}
+
 /*
  * ==============================================================================================================
  * pid, addr_start, addr_endから任意アドレス空間のrssを取得する
@@ -34,6 +70,8 @@ static int module_close(struct inode *inode, struct file *filp) {
 static long module_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) 
 {
     struct module_values* val = (struct module_values *)arg;
+
+    // pidからvmaを取得
     struct pid* pid = find_get_pid(val->pid);
     if (!pid) {
         printk("couldn't find pid %d's task\n", val->pid);
@@ -44,8 +82,15 @@ static long module_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
         printk("couldn't get task\n");
         return -1;
     }
+
+    // 任意アドレス空間rss取得
     struct vm_area_struct* vma = task->mm->mmap_base;
-    return 0;
+    struct mem_size_stats mss;
+
+    memset(&mss, 0, sizeof(mss));
+
+    my_smap_gather_stats(vma, &mss, val->addr_start, val->addr_end);
+    return mss->resident;
 }
 
 static struct file_operations module_fops = {
